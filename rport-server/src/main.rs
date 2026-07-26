a164612fd3b4cdc675e49cafc5c193623ecf8743
jinti <shenjindi@fourz.cn>
2026-07-26 09:39:13 +0800

    v0.1.11: silence noisy turn crate debug logs in --debug mode

--- rport-server/src/main.rs
+++ rport-server/src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> anyhow::Result<()> {
     let cli = ServerCli::parse();
 
     let log_filter = if cli.debug {
-        EnvFilter::new("debug,hyper=warn,gather=warn,igd=warn,neli=warn,rustls_platform_verifier=warn")
+        EnvFilter::new("debug,hyper=warn,gather=warn,igd=warn,neli=warn,rustls_platform_verifier=warn,turn=off")
     } else {
         EnvFilter::from_default_env()
     };
